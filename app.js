c65db01104e0445821b19ae9714fcf21ed37512f
Kiwai-cyber <2301100379dc@cyber-u.ac.jp>
2025-11-22 19:39:10 +0900

    Add dog API route and view

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');//追加
-var apiRouter = require('./routes/api');//追加
+var dogRouter = require('./routes/dog');//追加
 
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);//追加
-app.use('/api', apiRouter);//追加
+app.use('/dog', dogRouter);//追加
 
 
 // catch 404 and forward to error handler
