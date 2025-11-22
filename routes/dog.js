84f9cedef19822131229d497e799fa2d97676e00
Kiwai-cyber <2301100379dc@cyber-u.ac.jp>
2025-11-22 19:56:55 +0900

    Add dog route and view

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fetch = require('node-fetch');
 
 router.get('/', async function(req, res){
-    const response = await fetch('https://yesno.wtf/api');
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
     const data = await response.json();
     res.render('api', { title: 'YesNo API', answer: data.answer, image: data.image });
 });
